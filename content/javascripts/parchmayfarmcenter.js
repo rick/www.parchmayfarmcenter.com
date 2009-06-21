4150491cee6795d2760ba8f2162f1f16af5bd5b0
Rick Bradley <rick@rickbradley.com>
2009-06-21 17:52:42 -0500

    hovers for grid links

--- content/javascripts/parchmayfarmcenter.js
+++ content/javascripts/parchmayfarmcenter.js
@@ -1,8 +1,19 @@
 jQuery(function($) {
-//  $('.link-cell').hover(function() {
-//    
-//  });
+  
+  // hovers for cell images
+  var holder;
+  $('.link-cell > a > img').hover(
+    function() {
+      holder = this.src;
+      $(this).attr('src', this.src.replace(/\.png/, '-hover.png'));
+    }, 
+    function() {
+      $(this).attr('src', holder.replace(/-hover\.png/, '.png'));
+      holder = '';
+    }
+  );
 
+  // jquery load of content area on link click
   $('.link-cell a, a.content-link').click(function(event) { 
     event.preventDefault();
     $('#content-area').load(this.href);
