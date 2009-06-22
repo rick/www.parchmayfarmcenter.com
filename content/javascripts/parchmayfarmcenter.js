a5e3245ae7358ecc50f4f7becbd14cd21aed22e5
Rick Bradley <rick@rickbradley.com>
2009-06-21 19:23:58 -0500

    Grid gets replaced on loads now.  Adding "close" linkage to go back home.
    Livequery is the shit.

--- content/javascripts/parchmayfarmcenter.js
+++ content/javascripts/parchmayfarmcenter.js
@@ -2,20 +2,28 @@ jQuery(function($) {
   
   // hovers for cell images
   var holder;
-  $('.link-cell > a > img').hover(
-    function() {
+  $('.link-cell > a > img').livequery(function() {
+    $(this).hover(function() {
       holder = this.src;
       $(this).attr('src', this.src.replace(/\.png/, '-hover.png'));
     }, 
     function() {
       $(this).attr('src', holder.replace(/-hover\.png/, '.png'));
       holder = '';
-    }
-  );
+    });
+  });
 
   // jquery load of content area on link click
-  $('.link-cell a, a.content-link').click(function(event) { 
+  $('.link-cell a, a.content-link').livequery('click', function(event) { 
     event.preventDefault();
     $('#content-area').load(this.href);
+    $('#grid').load(this.href.replace(/\.html/, '-grid.html'));
+  });
+  
+  // "return home" links
+  $('a.home-link').livequery('click', function (event) { 
+    event.preventDefault();
+    $('#grid').load('/home-grid.html');
+    $('#content-area').load('/home-contents.html');
   });
 });
